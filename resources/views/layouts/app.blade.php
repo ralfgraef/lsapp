2c7be0a6c6bba7699cac87398c55b6da3d100426
Ralle1968 <graef.ralf@gmail.com>
2018-10-26 14:26:39 +0200

    add authentication

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,20 +1,35 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="{{ asset('css/app.css')  }}">
-    <title>{{ config('app.name', 'LSAPP') }}</title>
-  </head>
-  <body>
-    @include('inc.navbar')
-    <div class="container mt-4">
-      @include('inc.messages')
-      @yield('content')
-  </div>
-  <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+        @include('inc.navbar')
+          <div class="container">
+          <main class="py-4">
+            @include('inc.messages')
+            @yield('content')
+          </main>
+        </div>
+    <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
     <script>
         CKEDITOR.replace( 'article-ckeditor' );
-    </script>
-  </body>
+    </script>    
+</body>
 </html>
