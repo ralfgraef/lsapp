6140f1cf17ae5353fe2bcf95676f1c8a0ed1a02c
Ralle1968 <graef.ralf@gmail.com>
2018-10-29 17:07:10 +0100

    add close for message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,11 @@
                 icon: "{{ notify()->icon() }}",
             })    
         @endif
-        
+        window.setTimeout(function() {
+            $(".alert").fadeTo(500, 0).slideUp(500, function(){
+                $(this).remove(); 
+            });
+        }, 4000);
     </script>
 </body>
 </html>
