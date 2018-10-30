a3d0a576cb95f7a984cdae8c69b2a2a362ddc4eb
Ralle1968 <graef.ralf@gmail.com>
2018-10-30 16:35:27 +0100

    add access controll

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,6 +8,16 @@ use App\Post;
 class PostsController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
@@ -72,6 +82,11 @@ class PostsController extends Controller
     public function edit($id)
     {
         $post = Post::find($id);
+
+        // Check for correct user
+        if(auth()->user()->id != $post->user_id) {
+            return redirect('/posts')->with('error', 'Unauthorized page');
+        }
         return view('posts.edit')->with('post', $post);
     }
 
@@ -107,6 +122,12 @@ class PostsController extends Controller
     public function destroy($id)
     {
         $post = Post::find($id);
+
+        // Check for correct user
+        if(auth()->user()->id != $post->user_id) {
+            return redirect('/posts')->with('error', 'Unauthorized page');
+        }
+
         $post->delete();
 
         notify()->flash('You have deleted a post', 'success');
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,9 +7,14 @@
   <hr>
   <small>Written on {{ $post->created_at }} by {{ $post->user->name}}</small>
   <hr>
-  <a href="/posts/{{$post->id}}/edit" class="btn btn-outline-secondary">Edit</a>
-  {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right']) !!}
-    {{Form::hidden('_method', 'DELETE')}}
-    {{Form::submit('Delete', ['class' => 'btn btn-outline-danger'])}}
-  {!!Form::close() !!}
+  
+  @if (!Auth::guest())
+    @if(Auth::user()->id == $post->user_id)
+      <a href="/posts/{{$post->id}}/edit" class="btn btn-outline-secondary">Edit</a>
+      {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right']) !!}
+        {{Form::hidden('_method', 'DELETE')}}
+        {{Form::submit('Delete', ['class' => 'btn btn-outline-danger'])}}
+      {!!Form::close() !!}
+    @endif
+  @endif
 @endsection
